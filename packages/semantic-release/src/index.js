9176c2dc76c1d23b552ddfa6cfb47efe2fecea35
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-19 07:59:30 -0800

    fix: refactor index.js semantic release

--- packages/semantic-release/src/index.js
+++ packages/semantic-release/src/index.js
@@ -1,21 +1,21 @@
-{
-  "branches": [
+module.exports = {
+  branches: [
     "main",
     {
-      "name": "beta",
-      "prerelease": true
+      name: "beta",
+      prerelease: true,
     },
     {
-      "name": "alpha",
-      "prerelease": true
-    }
+      name: "alpha",
+      prerelease: true,
+    },
   ],
-  "plugins": [
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git"
-  ]
-}
+    "@semantic-release/git",
+  ],
+};
