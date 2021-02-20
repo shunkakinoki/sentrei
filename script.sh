4707396b4bff47931a637dde097dfbe35c327455
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-20 09:30:56 -0800

    perf: refactor scripts sh

--- script.sh
+++ script.sh
@@ -9,8 +9,15 @@ if [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ "[skip ci]" ]]; then
 elif [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" == "alpha" || "$VERCEL_GIT_COMMIT_REF" == "beta" || "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
   exit 1
 else
+  if [[ "$PWD" =~ "apps/demo" ]]; then
+    if git diff --quiet HEAD~ -- .; then
+      exit 0
+    else
+      exit 1
+    fi
+  fi
   if [[ "$PWD" =~ "apps/landing" ]]; then
-    if git diff --quiet HEAD~ -- .. ../../packages; then
+    if git diff --quiet HEAD~ -- .; then
       exit 0
     else
       exit 1
