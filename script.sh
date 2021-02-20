ce1cf2c0d28daa2f5d639acd76bb8f8dfea3e2e5
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-20 08:48:38 -0800

    perf: ini script sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ "[skip ci]" ]]; then
+  exit 0
+elif [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" == "alpha" || "$VERCEL_GIT_COMMIT_REF" == "beta" || "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+  exit 1
+else
+  if [[ "$PWD" =~ "apps/web" ]]; then
+    if git diff --quiet HEAD~ -- .. ../../packages; then
+      exit 0
+    else
+      exit 1
+    fi
+  fi
+fi
