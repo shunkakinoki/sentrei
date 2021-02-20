670131d0ff55ee6f97289a175c35a2b5d1532efa
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-20 07:57:16 -0800

    feat: ini apps web

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import type { FC } from "react";
+import { RecoilRoot } from "recoil";
+
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <RecoilRoot>
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </RecoilRoot>
+);
+
+export default App;
