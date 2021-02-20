1d664a1201cfb1c71ac9e4a0dbad0b85edf17d4d
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-20 04:17:41 -0800

    perf: add json lint-staged

--- configurations/lint-staged/src/index.js
+++ configurations/lint-staged/src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   "*.{js,ts,tsx}": ["yarn run eslint:fix", "yarn run prettier:fix"],
+  "*.json": ["yarn run prettier:fix"],
   "package.json": ["yarn run sort-package-json:fix"],
 };
