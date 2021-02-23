247a88077713d95e8c88db513f8b01c68d28b460
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-23 01:39:41 -0800

    perf: refactor eslint js

--- configurations/eslint/src/index.js
+++ configurations/eslint/src/index.js
@@ -28,6 +28,7 @@ module.exports = {
         "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/no-unsafe-call": "off",
         "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-var-requires": "off",
       },
     },
