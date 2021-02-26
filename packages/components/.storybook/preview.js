b1b5c50fb325783c02dc5b8419bf1906423d12a1
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-26 01:24:49 -0800

    perf: refactor story addons

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { ThemeProvider } from "../src/theme";
 
+export const parameters = {
+  controls: { expanded: true },
+};
+
 export const decorators = [
   Story => (
     <ThemeProvider>
--- packages/components/src/atoms/Button.stories.tsx
+++ packages/components/src/atoms/Button.stories.tsx
@@ -1,7 +1,12 @@
+import { action } from "@storybook/addon-actions";
+
 import { Button } from "./Button";
 
 export default {
+  component: Button,
   title: "Button",
 };
 
-export const PrimaryButton = (): JSX.Element => <Button>Primary</Button>;
+export const PrimaryButton = (): JSX.Element => (
+  <Button onClick={action("clicked")}>Primary</Button>
+);
