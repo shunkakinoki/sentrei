1891df316edc638642afb43a5db27cacd6cac7fe
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-27 09:52:58 -0800

    perf: ini landing screen

--- packages/components/src/screens/LandingScreen/LandingScreen.tsx
+++ packages/components/src/screens/LandingScreen/LandingScreen.tsx
@@ -1,10 +1,26 @@
 import type { FC } from "react";
 
+import { Faq } from "@sentrei/components/organisms/Faq";
+import { Feature } from "@sentrei/components/organisms/Feature";
+import { Globe } from "@sentrei/components/organisms/Globe";
 import { Hero } from "@sentrei/components/organisms/Hero";
+import { Journey } from "@sentrei/components/organisms/Journey";
+import { Mission } from "@sentrei/components/organisms/Mission";
+import { Newsletter } from "@sentrei/components/organisms/Newsletter";
+import { Stats } from "@sentrei/components/organisms/Stats";
+import { Testimonial } from "@sentrei/components/organisms/Testimonial";
 import { PageRoot } from "@sentrei/components/roots/PageRoot";
 
 export const LandingScreen: FC = () => (
   <PageRoot>
     <Hero />
+    <Journey />
+    <Feature />
+    <Testimonial />
+    <Mission />
+    <Globe />
+    <Stats />
+    <Faq />
+    <Newsletter />
   </PageRoot>
 );
