33501a9bf46e54b79a8070c90f30128e2189c6a3
Shun Kakinoki <shunkakinoki@gmail.com>
2021-03-18 06:10:56 -0700

    perf: refactor next translate

---
+++ apps/dashboard/i18n.js
@@ -0,0 +1,9 @@
+module.exports = {
+  defaultLocale: "en",
+  locales: ["en", "ja", "zh"],
+  pages: {
+    "*": ["common", "dialog", "error"],
+  },
+  loadLocaleFrom: (lang, ns) =>
+    import(`../../locales/${lang}/${ns}.json`).then(m => m.default),
+};
