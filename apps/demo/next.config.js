79085a37c6b44dd5f7f044fc0940f74d65aadeef
Shun Kakinoki <shunkakinoki@gmail.com>
2021-03-20 00:41:38 -0700

    feat: ini module exports

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -1,1 +1,3 @@
-module.exports = require("@sentrei/next");
+module.exports = Object.assign(require("@sentrei/next"), {
+  basePath: "/demo",
+});
--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,1 +1,24 @@
-module.exports = require("@sentrei/next");
+const { BASE_URL } = process.env;
+
+module.exports = Object.assign(require("@sentrei/next"), {
+  rewrites() {
+    return [
+      {
+        source: "/dashboard",
+        destination: `${BASE_URL}/dashboard`,
+      },
+      {
+        source: "/dashboard/:path*",
+        destination: `${BASE_URL}/dashboard/:path*`,
+      },
+      {
+        source: "/demo",
+        destination: `${BASE_URL}/demo`,
+      },
+      {
+        source: "/demo/:path*",
+        destination: `${BASE_URL}/demo/:path*`,
+      },
+    ];
+  },
+});
