e8f2bc7089f1b52d9126af309b37dc48080a4421
Shun Kakinoki <shunkakinoki@gmail.com>
2021-03-22 07:35:49 -0700

    perf: migrate react version to 17

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,1 +1,31 @@
-module.exports = require("@sentrei/next");
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+const withPlugins = require("next-compose-plugins");
+const withTranslate = require("next-translate");
+const withTM = require("next-transpile-modules")([
+  "@sentrei/atoms",
+  "@sentrei/const",
+  "@sentrei/molecules",
+  "@sentrei/organisms",
+  "@sentrei/roots",
+  "@sentrei/screens",
+  "@sentrei/themes",
+  "@sentrei/utils",
+]);
+
+const config = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ja", "zh"],
+  },
+  reactStrictMode: true,
+};
+
+const plugins = [
+  withBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+  }),
+  withTranslate,
+  withTM,
+];
+
+module.exports = withPlugins(plugins, config);
