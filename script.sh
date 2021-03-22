13889cd9bdb3e03ba8374b040237449487488271
Shun Kakinoki <shunkakinoki@gmail.com>
2021-03-22 08:33:57 -0700

    chore: update

--- script.sh
+++ script.sh
@@ -17,21 +17,21 @@ else
       exit 1
     fi
   fi
-  elif [[ "$PWD" =~ "apps/demo" ]]; then
+  if [[ "$PWD" =~ "apps/demo" ]]; then
     if git diff --quiet HEAD~ -- .; then
       exit 0
     else
       exit 1
     fi
   fi
-  elif [[ "$PWD" =~ "apps/home" ]]; then
+  if [[ "$PWD" =~ "apps/home" ]]; then
     if git diff --quiet HEAD~ -- .; then
       exit 0
     else
       exit 1
     fi
   fi
-  elif [[ "$PWD" =~ "(\/)tmp(\/).*" ]]; then
+  if [[ "$PWD" =~ "(\/)tmp(\/).*" ]]; then
     if git diff --quiet HEAD~ -- .; then
       exit 0
     else
