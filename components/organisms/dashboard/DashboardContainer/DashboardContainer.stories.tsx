ae6cba98d2354dba62f08546601c328c853ba4db
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-03-22 09:27:34 -0700

    Update DashboardContainer.stories.tsx

--- components/organisms/dashboard/DashboardContainer/DashboardContainer.stories.tsx
+++ components/organisms/dashboard/DashboardContainer/DashboardContainer.stories.tsx
@@ -4,7 +4,7 @@ import { DashboardContainer } from "./DashboardContainer";
 
 export default {
   component: DashboardContainer,
-  title: "Organisms/Landing/Dashboard/DashboardContainer",
+  title: "Organisms/Dashboard/DashboardContainer",
 };
 
 export const _DashboardContainer = (): JSX.Element => <DashboardContainer />;
