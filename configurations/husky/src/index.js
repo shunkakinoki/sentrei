99aea921d52b2bc9b28d11c55a30f5e01e8275fa
Shun Kakinoki <shunkakinoki@gmail.com>
2021-03-30 05:20:08 -0700

    chore: update

--- configurations/husky/src/index.js
+++ configurations/husky/src/index.js
@@ -1,8 +1,1 @@
-module.exports = {
-  hooks: {
-    "commit-msg": "commitlint -e $HUSKY_GIT_PARAMS",
-    "pre-commit": "lint-staged",
-    "prepare-commit-msg":
-      "exec < /dev/tty && git-cz -e --disable-emoji --hook || true",
-  },
-};
+export {};
