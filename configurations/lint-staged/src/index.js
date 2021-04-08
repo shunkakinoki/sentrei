15b791ed7c8dc285b4ce265607a1213758e38e68
Shun Kakinoki <shunkakinoki@gmail.com>
2021-04-07 17:37:58 -0700

    chore: update

--- configurations/lint-staged/src/index.js
+++ configurations/lint-staged/src/index.js
@@ -1,9 +1,5 @@
 module.exports = {
   "*.{js,ts,tsx}": ["yarn run eslint:fix", "yarn run prettier:fix"],
   "*.{md,json,yml}": ["yarn run prettier:fix"],
-  "package.json": [
-    "yarn run npm-package-json:lint",
-    "yarn run sort-package-json:fix",
-    "yarn run prettier:fix",
-  ],
+  "package.json": ["yarn run npm-package-json:lint", "yarn run prettier:fix"],
 };
