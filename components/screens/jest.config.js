7ab12f106068c80b4354efc69f49423449a69b00
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-23 09:17:12 -0700

    feat: ini components screens

---
+++ components/screens/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  displayName: "organisms",
+  preset: "../../jest.preset.js",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+    },
+  },
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "^@sentrei/atoms": "<rootDir>/../../components/atoms/src/index.ts",
+    "^@sentrei/molecules": "<rootDir>/../../components/molecules/src/index.ts",
+    "^@sentrei/organisms": "<rootDir>/../../components/organisms/src/index.ts",
+  },
+  coverageDirectory: "../../coverage/components/organisms",
+};
