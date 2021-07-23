b2c0bccf48fc0c645b34245494d2a2c2846f4113
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-23 11:03:52 -0700

    chore: update

--- script.sh
+++ script.sh
@@ -31,7 +31,6 @@ npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 IS_AFFECTED=$?
 
 if [ $IS_AFFECTED -eq 1 ]; then
-  date
   echo "🛑 - Build cancelled at $APP"
   exit 0
 elif [ $IS_AFFECTED -eq 0 ]; then
