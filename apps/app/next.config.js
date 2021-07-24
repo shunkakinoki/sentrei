e8afb4943a5189c10927cbd577d236882d6c1866
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-23 19:05:42 -0700

    perf: configure basepath requirements

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,1 +1,10 @@
-module.exports = require("@sentrei/next");
+const { defaultConfig, plugins, withPlugins } = require("@sentrei/next");
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = { ...defaultConfig, ...{ basePath: "/app" } };
+
+console.log(config);
+
+module.exports = withPlugins(plugins, config);
