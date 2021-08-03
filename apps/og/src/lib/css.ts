5eb016c0669a0ee70c14f7a77a55ec368f7d8809
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-02 18:56:53 -0700

    chore: update

--- apps/og/src/lib/css.ts
+++ apps/og/src/lib/css.ts
@@ -1,3 +1,50 @@
 export const css = string => {
   return string;
 };
+
+export const getCommonCSS = () => {
+  return css`
+    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
+
+    body {
+      background: white;
+      color: black;
+      background-size: 100px 100px;
+      height: 100vh;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      font-family: "Inter", sans-serif;
+      font-weight: 400;
+      font-size: 100px;
+      margin: 0;
+      padding: 0;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    p {
+      margin: 0;
+    }
+
+    code {
+      color: #d400ff;
+      font-family: "Vera";
+      white-space: pre-wrap;
+      letter-spacing: -5px;
+    }
+
+    code:before,
+    code:after {
+      content: "\`";
+    }
+  `;
+};
--- apps/og/src/lib/getHtml.tsx
+++ apps/og/src/lib/getHtml.tsx
@@ -1,59 +1,13 @@
 import { renderToString } from "react-dom/server";
 
+import { getCommonCSS } from "./css";
+
 import { layouts } from "@sentrei/og/layouts";
-import { css } from "@sentrei/og/lib";
 import type {
   IConfig,
   ILayoutConfig,
   LayoutComponent,
 } from "@sentrei/og/types";
-
-const getCommonCSS = () => {
-  return css`
-    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
-
-    body {
-      background: white;
-      color: black;
-      background-size: 100px 100px;
-      height: 100vh;
-      display: flex;
-      text-align: center;
-      align-items: center;
-      justify-content: center;
-      font-family: "Inter", sans-serif;
-      font-weight: 400;
-      font-size: 100px;
-      margin: 0;
-      padding: 0;
-    }
-
-    * {
-      box-sizing: border-box;
-    }
-
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    p {
-      margin: 0;
-    }
-
-    code {
-      color: #d400ff;
-      font-family: "Vera";
-      white-space: pre-wrap;
-      letter-spacing: -5px;
-    }
-
-    code:before,
-    code:after {
-      content: "\`";
-    }
-  `;
-};
 
 const NotImplemented: LayoutComponent = ({ config }) => {
   return <h1 style={{ fontSize: 100 }}>{config.layoutName} not implemented</h1>;
