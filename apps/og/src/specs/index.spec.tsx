de15b4c65bdafa6e0416d65edfb10b17ef677723
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-03 21:58:05 -0700

    chore: update

--- apps/og/src/specs/index.spec.tsx
+++ apps/og/src/specs/index.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
 
 import { Index } from "@sentrei/og/pages/index";
 
 describe("ButtonScreen", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <Index />
+      </RecoilRoot>,
+    );
     expect(baseElement).toBeTruthy();
   });
 });
