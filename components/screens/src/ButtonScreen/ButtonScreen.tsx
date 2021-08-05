cba0f00b7da046b3bb59136398debfc5f0d110ad
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-04 23:32:08 -0700

    chore: update

--- components/screens/src/ButtonScreen/ButtonScreen.tsx
+++ components/screens/src/ButtonScreen/ButtonScreen.tsx
@@ -1,16 +1,12 @@
 import { ButtonSection } from "@sentrei/organisms";
-import { AnalyticsRoot } from "@sentrei/roots";
 import type { VFC } from "react";
 
 export const ButtonScreen: VFC = () => {
   return (
-    <>
-      <AnalyticsRoot />
-      <div className="p-3 text-sky-300 bg-sky-300 rounded-lg">
-        <ButtonSection />
-        <ButtonSection />
-        <ButtonSection />
-      </div>
-    </>
+    <div className="p-3 text-sky-300 bg-sky-300 rounded-lg">
+      <ButtonSection />
+      <ButtonSection />
+      <ButtonSection />
+    </div>
   );
 };
