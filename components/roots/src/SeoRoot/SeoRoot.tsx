79fe428b5f16a85f424fa3bc3152745c96981540
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-04 23:03:25 -0700

    perf: ini og image

--- components/roots/src/SeoRoot/SeoRoot.tsx
+++ components/roots/src/SeoRoot/SeoRoot.tsx
@@ -1,19 +1,50 @@
-import type { DefaultSeoProps } from "next-seo";
-import { DefaultSeo } from "next-seo";
+import type { NextSeoProps } from "next-seo";
+import { DefaultSeo, NextSeo } from "next-seo";
+import Head from "next/head";
 import type { FC } from "react";
 
-export const SeoRoot: FC<DefaultSeoProps> = ({ ...props }) => {
+export type SeoRootProps = NextSeoProps & {
+  title?: string;
+  subTitle?: string;
+  description?: string;
+};
+
+export const SeoRoot: FC<SeoRootProps> = ({
+  title = "Sentrei",
+  subTitle = "All+in+one+edge+serverless+backend",
+  description = "Sentrei",
+  ...props
+}) => {
+  const image = `https://og.sentrei.com/api/image?fileType=png&layoutName=Sentrei&Theme=Dark&Title=${title}&Sub+Title=${subTitle}`;
+
   return (
-    <DefaultSeo
-      title="Sentrei"
-      description="Sentrei is a startup based in San Francisco."
-      titleTemplate="%s | Sentrei"
-      twitter={{
-        handle: "@SentreiHQ",
-        site: "@site",
-        cardType: "summary_large_image",
-      }}
-      {...props}
-    />
+    <>
+      <DefaultSeo
+        title="Sentrei"
+        description="Sentrei is a startup based in San Francisco."
+        titleTemplate="%s | Sentrei"
+        openGraph={{
+          type: "website",
+          url: "https://sentrei.com",
+          site_name: "Sentrei",
+          images: [{ url: image }],
+        }}
+        twitter={{
+          handle: "@SentreiHQ",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
+      <NextSeo
+        openGraph={{
+          images: [{ url: image }],
+        }}
+        {...props}
+      />
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+    </>
   );
 };
