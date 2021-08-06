6980156742dee643987a44c524acc4e99b4701ad
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-06 10:01:12 -0700

    perf: ini base path

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,10 +1,13 @@
 const { defaultConfig, plugins, withPlugins } = require("@sentrei/next");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
   ...defaultConfig,
+  basePath: isProduction ? "/app" : "/",
 };
 
 module.exports = withPlugins(plugins, config);
