8078627e80afae9af24806904e6a36cfb4476eb0
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-07 00:17:04 -0700

    chore: update

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -5,6 +5,30 @@ const { defaultConfig, plugins, withPlugins } = require("@sentrei/next");
  */
 const config = {
   ...defaultConfig,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/app/:path",
+        destination: "https://app.sentrei.com/:path",
+      },
+      {
+        source: "/demo/:path",
+        destination: "https://demo.sentrei.com/:path",
+      },
+      {
+        source: "/docs/:path",
+        destination: "https://docs.sentrei.com/:path",
+      },
+      {
+        source: "/og/:path",
+        destination: "https://og.sentrei.com/:path",
+      },
+    ];
+  },
 };
 
 module.exports = withPlugins(plugins, config);
