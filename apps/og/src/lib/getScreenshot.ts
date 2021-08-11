ca6de36f1a9e024214d9fca60cbab94fd12ebf90
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-10 18:05:58 -0700

    fix: remove chrome-aws-lambda package

--- apps/og/src/lib/getScreenshot.ts
+++ apps/og/src/lib/getScreenshot.ts
@@ -1,4 +1,3 @@
-import chrome from "chrome-aws-lambda";
 import type { Page } from "puppeteer-core";
 import * as core from "puppeteer-core";
 
@@ -7,43 +6,16 @@ import type { FileType } from "@sentrei/og/types";
 
 let _page: Page | null;
 
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
 interface Options {
   args: string[];
   executablePath: string;
   headless: boolean;
 }
 
-export const getOptions = async (isDev: boolean): Promise<Options> => {
-  let options: Options;
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath: exePath,
-      headless: true,
-    };
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    };
-  }
-
-  return options;
-};
-
-const getPage = async (isDev: boolean): Promise<Page> => {
+const getPage = async (isDev: boolean, options: Options): Promise<Page> => {
   if (_page) {
     return _page;
   }
-  const options = await getOptions(isDev);
   const browser = await core.launch(options);
   _page = await browser.newPage();
   return _page;
@@ -53,8 +25,9 @@ export const getScreenshot = async (
   html: string,
   type: FileType,
   isDev: boolean,
+  options: Options,
 ): Promise<string | void | Buffer> => {
-  const page = await getPage(isDev);
+  const page = await getPage(isDev, options);
   await page.setViewport({ width: OG_WIDTH, height: OG_HEIGHT });
   await page.setContent(html);
   const file = await page.screenshot({ type });
