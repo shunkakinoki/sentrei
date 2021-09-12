f4bdd0a67870cda1a6db9460da68ddb734d6fd41
Shun Kakinoki <shunkakinoki@gmail.com>
2021-09-12 02:30:20 -0700

    feat: ini twidaos com

--- apps/www-thisweekindaos-com/src/specs/index.spec.tsx
+++ apps/www-thisweekindaos-com/src/specs/index.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 
-import { ButtonScreen } from "@sentrei/demo/pages/index";
+import { ButtonScreen } from "@sentrei/www-thisweekindaos-com/pages/index";
 
 describe("ButtonScreen", () => {
   it("should render successfully", () => {
