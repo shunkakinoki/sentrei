0e00afa1d8617bfc73ff85c5ec82bd6a9fca47a2
Shun Kakinoki <shunkakinoki@gmail.com>
2021-09-12 00:56:48 -0700

    perf: ini blog sentrei com

--- apps/blog-sentrei-com/src/specs/index.spec.tsx
+++ apps/blog-sentrei-com/src/specs/index.spec.tsx
@@ -1,5 +1,6 @@
-import { ButtonScreen } from "@sentrei/sentrei/pages/index";
 import { render } from "@testing-library/react";
+
+import { ButtonScreen } from "@sentrei/blog-sentrei-com/pages/index";
 
 describe("ButtonScreen", () => {
   it("should render successfully", () => {
