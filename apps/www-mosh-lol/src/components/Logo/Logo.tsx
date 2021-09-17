95a383d509da197c9b984b94d18f4d3ebc22836e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-09-17 01:28:27 -0600

    perf: refactor config

--- apps/www-mosh-lol/src/components/Logo/Logo.tsx
+++ apps/www-mosh-lol/src/components/Logo/Logo.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 
-// eslint-disable-next-line no-restricted-imports
-import LogoImage from "../../../public/Logo.png";
-
 import styles from "./Logo.module.css";
+
+import LogoImage from "@sentrei/www-mosh-lol/public/Logo.png";
 
 export const Logo = () => {
   return (
--- apps/www-mosh-lol/src/pages/index.tsx
+++ apps/www-mosh-lol/src/pages/index.tsx
@@ -1,20 +1,16 @@
-/* eslint-disable no-restricted-imports */
-
 import moment from "moment";
-
 import dynamic from "next/dynamic";
-
 import Image from "next/image";
-
-import BlueHandImage from "../../public/BlueHand.png";
-import LogoImage from "../../public/Logo.png";
-import PurpleHandImage from "../../public/PurpleHand.png";
-import RedWaveImage from "../../public/RedWave.png";
-import SunshineImage from "../../public/Sunshine.png";
 
 import { CountdownClock } from "@sentrei/www-mosh-lol/components/CountdownClock";
 import { Footer } from "@sentrei/www-mosh-lol/components/Footer";
 import { Logo } from "@sentrei/www-mosh-lol/components/Logo";
+
+import BlueHandImage from "@sentrei/www-mosh-lol/public/BlueHand.png";
+import LogoImage from "@sentrei/www-mosh-lol/public/Logo.png";
+import PurpleHandImage from "@sentrei/www-mosh-lol/public/PurpleHand.png";
+import RedWaveImage from "@sentrei/www-mosh-lol/public/RedWave.png";
+import SunshineImage from "@sentrei/www-mosh-lol/public/Sunshine.png";
 
 const Canvas = dynamic(async () => {
   const m = await import("react-three-fiber");
