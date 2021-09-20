b8457c0020d7e9d5314064e1948edbe8184a15d1
Shun Kakinoki <shunkakinoki@gmail.com>
2021-09-20 12:33:39 -0700

    chore: update

--- components/atoms/src/Image/Image.stories.tsx
+++ components/atoms/src/Image/Image.stories.tsx
@@ -16,6 +16,7 @@ _Image.args = {
   alt: "Image",
   width: 100,
   height: 100,
+  // prettier-ignore
   src: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   fallbackSrc: "https://m.media-amazon.com/images/I/416FP6pKJPL._AC_.jpg",
 };
