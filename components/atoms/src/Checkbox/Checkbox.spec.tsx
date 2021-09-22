e4006f39d2209f10784386ad191c2d596442bcf5
wakkihaya <wakkihaya@gmail.com>
2021-09-22 15:56:45 +0900

    refactor: remove TODO in atoms about eslint-disable no-undef

--- components/atoms/src/Checkbox/Checkbox.spec.tsx
+++ components/atoms/src/Checkbox/Checkbox.spec.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-//TODO: remove after merging https://github.com/sentrei/sentrei/pull/273.
 import { render } from "@testing-library/react";
 
 import { Checkbox } from "./Checkbox";
