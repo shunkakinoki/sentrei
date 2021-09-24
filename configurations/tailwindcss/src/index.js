9c824bafa2147b184a4f959fc24c48479da5ee69
Shun Kakinoki <shunkakinoki@gmail.com>
2021-09-24 00:06:43 -0700

    chore: update

--- configurations/tailwindcss/src/index.js
+++ configurations/tailwindcss/src/index.js
@@ -19,6 +19,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+      },
       colors: {
         amber: colors.amber,
         blueGray: colors.blueGray,
@@ -36,6 +42,48 @@ module.exports = {
         trueGray: colors.trueGray,
         violet: colors.violet,
         warmGray: colors.warmGray,
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
       },
     },
   },
