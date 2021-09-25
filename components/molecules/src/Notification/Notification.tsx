67814403c43a4c5103068e1841d87f9c16fa8987
wakkihaya <wakkihaya@gmail.com>
2021-09-26 08:30:47 +0900

    fix: useToastNotification as a hook

--- components/molecules/src/Notification/Notification.tsx
+++ components/molecules/src/Notification/Notification.tsx
@@ -4,10 +4,6 @@ import clsx from "clsx";
 
 import type { FC } from "react";
 import { useEffect } from "react";
-import { useRecoilState } from "recoil";
-
-// eslint-disable-next-line no-restricted-imports
-import { isToastShowingState } from "../../../models/recoilState"; //TODO: set Path.
 
 import styles from "./Notification.module.scss";
 
@@ -39,13 +35,13 @@ export const Notification: FC<NotificationProps> = props => {
     title = "Notification",
     description = "Notify your status...",
   } = props;
-  const [isToastShowing, setIsToastShowing] =
-    useRecoilState(isToastShowingState);
+
+  const [isToastOpen, setToastOpen] = useToastNotification();
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (isAutoDelete) {
-        setIsToastShowing(false);
+        setToastOpen(false);
       }
     }, autoDeleteTime * 1000);
     return () => {
@@ -58,12 +54,12 @@ export const Notification: FC<NotificationProps> = props => {
       <div className={clsx("relative w-screen h-screen")}>
         <Button
           onClick={(): void => {
-            setIsToastShowing(true);
+            setToastOpen(true);
           }}
         >
           Info
         </Button>
-        {isToastShowing && (
+        {isToastOpen && (
           <div className={clsx("w-full h-full")}>
             <Toast
               title={title}
@@ -73,7 +69,7 @@ export const Notification: FC<NotificationProps> = props => {
               textColor={toastTextColor}
               className={clsx("absolute", styles[position])}
               onClick={(): void => {
-                setIsToastShowing(false);
+                setToastOpen(false);
               }}
             />
           </div>
