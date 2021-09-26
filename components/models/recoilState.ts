5b44b292bbd8c30acf72f52c49d18da89db152a5
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-09-25 17:53:33 -0700

    Delete recoilState.ts

--- components/models/recoilState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-
-//Atom for managing toast showing in Notification molecules.
-export const isToastShowingState = atom({
-  key: "isToastShowingState",
-  default: false,
-});
