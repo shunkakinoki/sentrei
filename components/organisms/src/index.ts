4c94ca1589aaba668a2b3f08d798d25bd476a73e
wakkihaya <wakkihaya@gmail.com>
2021-09-27 09:35:58 +0900

    refactor: remove NavBar from index.ts

--- components/organisms/src/index.ts
+++ components/organisms/src/index.ts
@@ -3,4 +3,3 @@ export { ButtonSection } from "./ButtonSection";
 export { FooterLogo } from "./FooterLogo";
 export { CountdownClock } from "./CountdownClock";
 export { VaporGrid } from "./VaporGrid";
-export { NavBar } from "./NavBar";
