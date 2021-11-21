c67871beb6e41836eefdd442af9291fc517b3bb4
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-20 19:55:52 -0500

    perf: ini zod app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: "@sentrei",
+  overrides: [
+    {
+      files: ["apps/app/prisma/zod/**"],
+      rules: {
+        "prettier/prettier": "off",
+        "import/order": "off",
+        "no-restricted-imports": "off",
+        "no-unused-vars": "off",
+      },
+    },
+  ],
 };
