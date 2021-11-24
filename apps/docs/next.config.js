39bdd796b41b2e844d46d3de643aabd48931dfce
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-23 23:24:45 -0800

    chore: update

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -6,6 +6,20 @@ const { defaultConfig } = require("@sentrei/next");
 const config = {
   ...defaultConfig,
   basePath: "/docs",
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "*.vercel.app",
+          },
+        ],
+        destination: "https://app.sentrei.com/docs",
+      },
+    ];
+  },
 };
 
 module.exports = config;
