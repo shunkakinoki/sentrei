9e7b195eee0feb21ffab60093e8c62b69af4bd0a
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-23 17:49:04 -0800

    feat: ini middleware rewrite

---
+++ apps/app/src/pages/_middleware.tsx
@@ -0,0 +1,16 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export const middleware = (req: NextRequest) => {
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("home")) {
+    return NextResponse.rewrite("home.sentrei.com");
+  }
+
+  if (pathname.startsWith("home")) {
+    return NextResponse.rewrite("home.sentrei.com");
+  }
+};
+
+export default middleware;
