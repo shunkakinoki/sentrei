d3bab3dbbeb81d6cd2071e3fdc4462e98d713b8c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-27 18:27:03 -0800

    chore: update

--- apps/docs/src/pages/_middleware.tsx
+++ apps/docs/src/pages/_middleware.tsx
@@ -2,27 +2,14 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export const middleware = (req: NextRequest) => {
-  const hostname = req.headers.get("host");
   const path = req.nextUrl.href.split("/")[1];
 
-  console.log(`hostname: ${hostname}`);
-  console.log(`path: ${path}`);
-  console.log(hostname.endsWith("vercel.app"));
+  if (path === "") {
+    return NextResponse.redirect("/docs");
+  }
 
   if (path === "favicon.svg") {
     return NextResponse.redirect("/docs/favicon.svg");
-  }
-
-  if (["api", "docs"].includes(path)) {
-    return;
-  } else {
-    if (
-      hostname.endsWith("vercel.app") ||
-      hostname === "docs.sentrei.com" ||
-      (process.env.NODE_ENV !== "production" && hostname === "localhost:3000")
-    ) {
-      return NextResponse.redirect("/docs");
-    }
   }
 };
 
