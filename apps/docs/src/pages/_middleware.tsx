c8178eab95b66cf0074ec825d83c354560104f95
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-27 18:34:37 -0800

    chore: update

--- apps/docs/src/pages/_middleware.tsx
+++ apps/docs/src/pages/_middleware.tsx
@@ -4,10 +4,6 @@ import { NextResponse } from "next/server";
 export const middleware = (req: NextRequest) => {
   const path = req.nextUrl.href.split("/")[1];
 
-  if (path === "") {
-    return NextResponse.redirect("/docs");
-  }
-
   if (path === "favicon.svg") {
     return NextResponse.redirect("/docs/favicon.svg");
   }
