0cad20e425aa9ea1aaf505deeff67fabc370b19e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-03 21:14:35 -0500

    perf: ini cypress app

--- configurations/eslint/src/index.js
+++ configurations/eslint/src/index.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:cypress/recommended",
     "plugin:import/errors",
     "plugin:import/typescript",
     "plugin:import/warnings",
