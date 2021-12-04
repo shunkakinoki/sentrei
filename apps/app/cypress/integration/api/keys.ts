395f2aa58992316883d36ecc6ba903be18fd4ad8
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-04 15:00:06 -0500

    perf: ini key testing

--- apps/app/cypress/integration/api/keys.ts
+++ apps/app/cypress/integration/api/keys.ts
@@ -7,8 +7,48 @@ describe("Index", () => {
       url: "/api/keys",
     }).should(response => {
       console.log(response);
+      console.log(response.headers);
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property("apiKeys");
+      expect(response.body.apiKeys.length).to.eq(0);
+    });
+  });
+  it("PUT", () => {
+    cy.request({
+      method: "PUT",
+      url: "/api/keys",
+    }).should(response => {
+      console.log(response);
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("done");
+    });
+  });
+  it("DELETE", () => {
+    cy.request({
+      method: "GET",
+      url: "/api/keys",
+    }).then(response => {
+      console.log(response);
+      const key = response.body.apiKeys[0][0];
+      cy.request({
+        method: "DELETE",
+        url: "/api/keys",
+        qs: {
+          key: key,
+        },
+      }).should(response => {
+        console.log(response);
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("done");
+      });
+    });
+    cy.request({
+      method: "GET",
+      url: "/api/keys",
+    }).should(response => {
+      console.log(response);
+      expect(response.status).to.eq(200);
+      expect(response.body.apiKeys.length).to.eq(0);
     });
   });
 });
